fda0470a85ef41cb78dfa2ff83fe2e8edf0ac5ac
Raphael Benoliel <raphael2gb@gmail.com>
2024-03-09 11:21:13 +0200

    fix selenium-test

--- Tests/selenium-test.spec.js
+++ Tests/selenium-test.spec.js
@@ -1,40 +1,28 @@
+/* eslint-disable no-console */
 import { Builder, By, until } from 'selenium-webdriver';
-import chrome from 'selenium-webdriver/chrome.js';
-import { describe, it, before, after } from 'mocha';
-import { expect } from 'chai';
+import assert from 'assert';
 
-let driver;
+(async function teststudnet() {
+  let driver;
 
-describe('Login Functionality Tests', function () {
-    this.timeout(30000); // Extend timeout for asynchronous operations
+  try {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('http://localhost:3000/');
+    const title = await driver.getTitle();
+    assert.equal(title, 'StudNet - Social Network');
 
-    before(async () => {
-        // Ensure 'chromedriver' is correctly set up in the path
-        await import('chromedriver');
+    await driver.findElement(By.css('input[type="email"]')).sendKeys('raphael2gb@gmail.com');
+    await driver.findElement(By.css('input[type="password"]')).sendKeys('Raphael1');
+    await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
 
-        // Initialize the WebDriver with Chrome in headless mode
-        const options = new chrome.Options();
-        options.addArguments('--headless'); // Correct way to enable headless mode
-        driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+    await driver.wait(until.urlIs('http://localhost:3000/#/home'), 10000);
+    const currentUrl = await driver.getCurrentUrl();
+    assert.equal(currentUrl, 'http://localhost:3000/#/home');
 
-        // driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-    });
-
-    after(async () => {
-        if (driver) {
-            await driver.quit();
-        }
-    });
-
-    it('Successful login redirects to home', async function () {
-        await driver.get('http://localhost:3000/');
-        await driver.findElement(By.css('input[type="email"]')).sendKeys('raphael2gb@gmail.com');
-        await driver.findElement(By.css('input[type="password"]')).sendKeys('Raphael1');
-        await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
-
-        await driver.wait(until.urlIs('http://localhost:3000/home'), 10000);
-        const currentUrl = await driver.getCurrentUrl();
-        
-        expect(currentUrl).to.equal('http://localhost:3000/#/home');
-    });
-});
+    console.log('Test passed!');
+  } catch (e) {
+    console.error('Test failed:', e);
+  } finally {
+    await driver.quit();
+  }
+}());
