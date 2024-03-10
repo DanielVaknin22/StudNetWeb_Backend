1c4b3dad383eacb51b57c9aa20433915cbaf04d2
Raphael Benoliel <raphael2gb@gmail.com>
2024-03-10 16:36:07 +0200

    CircleCI fix

--- Tests/selenium-test.spec.js
+++ Tests/selenium-test.spec.js
@@ -1,28 +1,28 @@
-// /* eslint-disable no-console */
-// import { Builder, By, until } from 'selenium-webdriver';
-// import assert from 'assert';
+/* eslint-disable no-console */
+import { Builder, By, until } from 'selenium-webdriver';
+import assert from 'assert';
 
-// (async function teststudnet() {
-//   let driver;
+(async function teststudnet() {
+  let driver;
 
-//   try {
-//     driver = await new Builder().forBrowser('chrome').build();
-//     await driver.get('http://localhost:3000/');
-//     const title = await driver.getTitle();
-//     assert.equal(title, 'StudNet - Social Network');
+  try {
+    driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('http://localhost:3000/');
+    const title = await driver.getTitle();
+    assert.equal(title, 'StudNet - Social Network');
 
-//     await driver.findElement(By.css('input[type="email"]')).sendKeys('raphael2gb@gmail.com');
-//     await driver.findElement(By.css('input[type="password"]')).sendKeys('Raphael1');
-//     await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
+    await driver.findElement(By.css('input[type="email"]')).sendKeys('raphael2gb@gmail.com');
+    await driver.findElement(By.css('input[type="password"]')).sendKeys('Raphael1');
+    await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
 
-//     await driver.wait(until.urlIs('http://localhost:3000/#/home'), 10000);
-//     const currentUrl = await driver.getCurrentUrl();
-//     assert.equal(currentUrl, 'http://localhost:3000/#/home');
+    await driver.wait(until.urlIs('http://localhost:3000/#/home'), 10000);
+    const currentUrl = await driver.getCurrentUrl();
+    assert.equal(currentUrl, 'http://localhost:3000/#/home');
 
-//     console.log('Test passed!');
-//   } catch (e) {
-//     console.error('Test failed:', e);
-//   } finally {
-//     await driver.quit();
-//   }
-// }());
+    console.log('Test passed!');
+  } catch (e) {
+    console.error('Test failed:', e);
+  } finally {
+    await driver.quit();
+  }
+}());
