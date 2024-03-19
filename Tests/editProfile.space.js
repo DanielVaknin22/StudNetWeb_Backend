c3a63d6995dce5b7d9ddb9020a23e397e41623dd
Maor Hadad <maorhadad94@gmail.com>
2024-03-19 22:44:44 +0200

    editing profile test

--- Tests/editProfile.space.js
+++ Tests/editProfile.space.js
@@ -11,8 +11,8 @@ import assert from 'assert';
       assert.equal(title, 'StudNet - Social Network');
   
       // login and check if the user is redirected to the home page
-      await driver.findElement(By.css('input[type="email"]')).sendKeys('raphael2gb@gmail.com');
-      await driver.findElement(By.css('input[type="password"]')).sendKeys('Raphael1');
+      await driver.findElement(By.css('input[type="email"]')).sendKeys('maorhadad94@gmail.com');
+      await driver.findElement(By.css('input[type="password"]')).sendKeys('Maor123456789');
       await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
   
       await driver.wait(until.urlIs('http://localhost:3000/#/home'), 10000);
@@ -21,9 +21,28 @@ import assert from 'assert';
   
       // Getting to editing a profile through the navBar
       await driver.get('http://localhost:3000/#/profile');
-    //   await driver.findElement(By.css('a.sc-iAEyYk.eNucbM')).click();
-      await driver.sleep(10000)
       await driver.findElement(By.css('button.sc-bSkxYT.dWSFze')).click();
+      
+      // Editing the profile
+      await driver.findElement(By.css('button.accountButton')).click();
+      await driver.findElement(By.css('input[name="userName"]')).clear();
+      await driver.findElement(By.css('input[name="userName"]')).sendKeys('BarGg');
+      await driver.findElement(By.css('input[name="firstName"]')).clear();
+      await driver.findElement(By.css('input[name="firstName"]')).sendKeys('Bar');
+      await driver.findElement(By.css('input[name="lastName"]')).clear();
+      await driver.findElement(By.css('input[name="lastName"]')).sendKeys('Cohen');
+      await driver.findElement(By.css('button.accountButton')).click();
+      await driver.sleep(5000)
+      
+      // Editing the password
+      await driver.findElement(By.css('react-tabs__tab-list')).click();
+      await driver.findElement(By.css('button.Password')).click();
+      await driver.sleep(5000);
+      await driver.findElement(By.css('input[name="currentPassword"]')).sendKeys('Maor123456789');
+      await driver.findElement(By.css('input[name="newPassword"]')).sendKeys('Maor1122334455');
+      await driver.findElement(By.css('input[name="confirmPassword"]')).sendKeys('Maor1122334455');
+      await driver.findElement(By.css('button.saveButton')).click();
+
       
     //   await driver.sleep(9000); // Wait for 2 seconds
     //   const post = await driver.findElement(By.css('h3.sc-fbJfA.sc-fGFwAa.eQTEMi.EdAWX'));
