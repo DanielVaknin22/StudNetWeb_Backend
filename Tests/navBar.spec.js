0de752dd10113698283b6d423ca2b2196bdb7337
MaorHadadLD <maorhadad94@gmail.com>
2024-05-24 16:28:50 +0300

    nav bar tests

---
+++ Tests/navBar.spec.js
@@ -0,0 +1,45 @@
+import { Builder, By, until } from 'selenium-webdriver';
+import assert from 'assert';
+
+(async function navBar() {
+    let driver;
+
+    try {
+        driver = await new Builder().forBrowser('chrome').build();
+    await driver.get('http://localhost:3000/');
+    const title = await driver.getTitle();
+    assert.equal(title, 'StudNet - Social Network');
+
+    // login and check if the user is redirected to the home page
+    await driver.findElement(By.css('input[type="email"]')).sendKeys('maorhadad94@gmail.com');
+    await driver.findElement(By.css('input[type="password"]')).sendKeys('Maor1122334455');
+    await driver.findElement(By.css('button.sc-eACynP.ipNQrM')).click();
+
+    await driver.wait(until.urlIs('http://localhost:3000/#/home'), 10000);
+    const currentUrl = await driver.getCurrentUrl();
+    assert.equal(currentUrl, 'http://localhost:3000/#/home');
+
+    // Getting to Nav Bar
+    await driver.sleep(1000);
+    await driver.get('http://localhost:3000/#/profile');
+    await driver.sleep(2000);
+    await driver.get('http://localhost:3000/#/search');
+    await driver.sleep(2000);
+    await driver.get('http://localhost:3000/#/tools');
+    await driver.sleep(2000);
+    await driver.get('http://localhost:3000/#/terms');
+    await driver.sleep(2000);
+    await driver.get('http://localhost:3000/#/about');
+    await driver.sleep(2000);
+    await driver.get('http://localhost:3000/#/terms');
+    
+    
+
+    
+    console.log('Test passed!');
+} catch (e) {
+  console.error('Test failed:', e);
+} finally {
+  await driver.quit();
+}
+}());
